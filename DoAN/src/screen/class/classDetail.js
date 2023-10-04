47321d3d42ab28f1a8900d86038b07412de1ccb2
Cuongcute <ngobacuong2211@gmail.com>
2023-10-04 16:53:15 +0700

    update api class Detail

--- DoAN/src/screen/class/classDetail.js
+++ DoAN/src/screen/class/classDetail.js
@@ -13,6 +13,7 @@ import { useRoute } from '@react-navigation/native';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { ActivityIndicator } from 'react-native-paper';
+import { FontAwesome } from '@expo/vector-icons';
 
 
 const CARD_WIDTH = sizes.width - 20;
@@ -37,10 +38,10 @@ const styles = StyleSheet.create({
     },
     textOnImage: {
         position: 'absolute',
-        top: '50%',
-        left: '50%',
+        top: '30%',
+        left: '40%',
         transform: [{ translateX: -50 }, { translateY: -50 }],
-        color: 'white',
+        color: 'black',
         fontSize: 30,
         fontWeight: 'bold',
     },
@@ -116,6 +117,30 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         borderRadius: 10,
     },
+    DetailContainer: {
+        width: 300,
+        height: CARD_HEIGHT - 30,
+        borderRadius: 20,
+        backgroundColor: colors.black,
+        alignSelf: 'center',
+
+    },
+    detailText: {
+        textAlign: 'justify',
+        marginHorizontal: 20,
+        marginTop: 5,
+    },
+    feeText: {
+        color: colors.gray,
+        fontWeight: 'bold',
+        fontSize: 20, // Điều chỉnh kích thước văn bản tùy theo nhu cầu của bạn
+        textAlign: 'center', // Để canh giữa văn bản theo chiều ngang
+        transform: [{ translateX: -50 }, { translateY: -50 }],
+        position: 'absolute',
+        top: '60%',
+        left: '50%',
+    }
+
 });
 
 export default function classDetail(props) {
@@ -123,9 +148,16 @@ export default function classDetail(props) {
 
     const navigation = useNavigation();
 
+    const [showButtons, setShowButtons] = useState(false);
+
+
     const handleBack = () => {
         navigation.navigate('Home');
     };
+
+    const handleEnroll = () => {
+        setShowButtons(true);
+    }
 
     const route = useRoute();
     const { classId } = route.params; // Lấy ID từ route.params
@@ -135,18 +167,16 @@ export default function classDetail(props) {
     const [isLoading, setIsLoading] = useState(true);
 
 
-    const URL = 'https://ade4-123-24-215-136.ngrok-free.app';
+    const URL = 'https://c48f-123-24-215-136.ngrok-free.app';
 
     useEffect(() => {
         async function getClassById() {
             const response = await fetch(URL + '/api/Class/GetClassById/' + classId);
             const JsonConvert = await response.json();
             setClassData(JsonConvert);
-            console.log(classId);
             setIsLoading(false);
 
         }
-        console.log(classId);
         getClassById();
     });
 
@@ -158,20 +188,28 @@ export default function classDetail(props) {
                         style={styles.image}
                         source={{ uri: URL + '/GetImage/' + classData[0]?.courseId }}
                     />
-                    <Text style={styles.textOnImage}>{classData[0]?.fee}</Text>
+                    <Text style={styles.textOnImage}>ENGLISH_490</Text>
+
+                    <Text style={styles.feeText}>Fee : {classData[0]?.fee}</Text>
+
                     <TouchableOpacity style={styles.Arrowback} onPress={handleBack}>
                         <Ionicons name="arrow-back-circle-outline" size={30} color="white" />
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={[styles.actionButton, styles.report]}>
-                        <Text style={styles.buttonText}>Report </Text>
-                        <MaterialIcons style={styles.reportIcon} name="report" size={17} color="white" />
-                    </TouchableOpacity>
+                    {showButtons && (
 
-                    <TouchableOpacity style={[styles.actionButton, styles.enroll]}>
-                        <Text style={styles.buttonText}>Enroll </Text>
-                        <AntDesign name="pluscircleo" size={15} color="white" />
-                    </TouchableOpacity>
+                        <TouchableOpacity style={[styles.actionButton, styles.report]}>
+                            <Text style={styles.buttonText}>Report </Text>
+                            <MaterialIcons style={styles.reportIcon} name="report" size={17} color="white" />
+                        </TouchableOpacity>
+                    )}
+                    {!showButtons && (
+
+                        <TouchableOpacity style={[styles.actionButton, styles.enroll]} onPress={handleEnroll}>
+                            <Text style={styles.buttonText}>Enroll </Text>
+                            <AntDesign name="pluscircleo" size={15} color="white" />
+                        </TouchableOpacity>
+                    )}
                 </View>
             )}
 
@@ -179,45 +217,78 @@ export default function classDetail(props) {
                 <>
                     <Text style={styles.des}>Description</Text>
                     <Text style={styles.desText}>
-                        The Basic English Communication Class is a course that provides students with fundamental knowledge and skills to confidently communicate in various everyday situations. This course is designed for learners at a basic level and does not require prior knowledge of English.
+                        {classData[0]?.description}
                     </Text>
 
-                    <View style={styles.buttonContainer}>
-                        <TouchableOpacity>
-                            <LinearGradient colors={['#0093E9', '#80D0C7']} style={styles.linearGradient}>
-                                <Feather name="message-circle" size={24} color="white" />
-                                <Text style={styles.buttonText}>Group chat</Text>
-                            </LinearGradient>
-                        </TouchableOpacity>
 
-                        <TouchableOpacity>
-                            <LinearGradient colors={['#FF3CAC', '#784BA0', '#2B86C5']} style={styles.linearGradient}>
-                                <MaterialIcons name="assignment" size={24} color="white" />
-                                <Text style={styles.buttonText}>Quiz</Text>
-                            </LinearGradient>
-                        </TouchableOpacity>
-                    </View>
+                    <LinearGradient colors={['#F7DBA7', '#F0AB86']} style={styles.DetailContainer}>
 
-                    <TouchableOpacity style={styles.meetingRoomContainer}>
-                        <LinearGradient colors={['#ED213A', '#93291E']} style={styles.meetingRoom}>
-                            <FontAwesome5 name="chalkboard-teacher" size={24} color="white" />
-                            <Text style={styles.buttonText}>Meeting room</Text>
-                        </LinearGradient>
-                    </TouchableOpacity>
+                        <Text style={styles.des}>Class Deatail</Text>
+                        <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+                            <MaterialIcons name="date-range" size={20} color="black" style={{ marginTop: 5 }} />
+                            <Text style={[styles.detailText, { color: colors.gray, fontWeight: 'bold', alignSelf: 'center', fontSize: 13 }]}>
+                                Start Date: {classData[0]?.startDate ? new Date(classData[0]?.startDate).toLocaleDateString('en-GB') : ''}
+                            </Text>
+                        </View>
 
-                    <Text>{classData[0]?.className}</Text>
+                        <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+                            <MaterialIcons name="date-range" size={20} color="black" style={{ marginTop: 5 }} />
 
+                            <Text style={[styles.detailText, { color: colors.gray, fontWeight: 'bold', alignSelf: 'center', fontSize: 13 }]}>
+                                End Date  : {classData[0]?.endDate ? new Date(classData[0]?.startDate).toLocaleDateString('en-GB') : ''}
+                            </Text>
+                        </View>
+                        <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+                        <AntDesign name="team" size={20} color="black" style={{ marginTop: 5 }}   />
+                            <Text style={[styles.detailText, { color: colors.gray, fontWeight: 'bold', alignSelf: 'center', fontSize: 13 }]}>
+                                Student In  Class  :   {classData[0]?.numberStudent}
+                            </Text>
+                        </View>
+
+                        <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+                        <FontAwesome5 name="calendar-week" size={20} color="black"  style={{ marginTop: 5 }} />
+                        <Text style={[styles.detailText, { color: colors.gray, fontWeight: 'bold', alignSelf: 'center', fontSize: 13 }]}>
+                            Number Of Week :   {classData[0]?.numberOfWeek}
+                        </Text>
+                        </View>
+                    </LinearGradient>
+
+                    {showButtons && (
+                        <>
+                            <View style={styles.buttonContainer}>
+                                <TouchableOpacity>
+                                    <LinearGradient colors={['#0093E9', '#80D0C7']} style={styles.linearGradient}>
+                                        <Feather name="message-circle" size={24} color="white" />
+                                        <Text style={styles.buttonText}>Group chat</Text>
+                                    </LinearGradient>
+                                </TouchableOpacity>
+
+                                <TouchableOpacity>
+                                    <LinearGradient colors={['#FF3CAC', '#784BA0', '#2B86C5']} style={styles.linearGradient}>
+                                        <MaterialIcons name="assignment" size={24} color="white" />
+                                        <Text style={styles.buttonText}>Quiz</Text>
+                                    </LinearGradient>
+                                </TouchableOpacity>
+                            </View>
+
+                            <TouchableOpacity style={styles.meetingRoomContainer}>
+                                <LinearGradient colors={['#ED213A', '#93291E']} style={styles.meetingRoom}>
+                                    <FontAwesome5 name="chalkboard-teacher" size={24} color="white" />
+                                    <Text style={styles.buttonText}>Meeting room</Text>
+                                </LinearGradient>
+                            </TouchableOpacity>
+                        </>
+                    )}
                 </>
             )}
+
             {isLoading && (
                 <View>
                     <View style={styles.imageBox}>
-
                         <ActivityIndicator size="medium" color={colors.primary} />
                     </View>
                 </View>
             )}
         </ScrollView>
-
     );
 }
