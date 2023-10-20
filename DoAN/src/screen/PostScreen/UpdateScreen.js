af2673b732edec8e7098f742bbebd7ed10fe83c8
Cuongcute <ngobacuong2211@gmail.com>
2023-10-20 20:57:38 +0700

    update

--- DoAN/src/screen/PostScreen/UpdateScreen.js
+++ DoAN/src/screen/PostScreen/UpdateScreen.js
@@ -1,11 +1,45 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { useEffect } from "react";
+import UpdateList from "./UpdateList";
+
+import User from "../../user";
+import myGlobalVariable from "../../global";
+
 
 export default function UpdateScreen({ navigation }) {
 
+
+    const URL = myGlobalVariable;
+
+    const [AllPost, setAllPost] = React.useState([]);
+
+
+
+    useEffect(() => {
+        async function getUser() {
+            try {
+                const response = await fetch(URL + '/api/Post/GetUserPost/' + User);
+                if (response.ok) {
+                    const post = await response.json();
+                    setAllPost(post);
+                }
+
+            } catch (error) {
+                console.error(error);
+            } finally {
+                console.log(AllPost);
+
+                setLoading(false);
+            }
+        }
+
+        getUser();
+    }, []);
+
     return (
-        <View style={{ backgroundColor: "red", height:600 }}>
-            <Text>Hello world</Text>
+        <View>
+            <UpdateList posts={AllPost}/>
         </View>
     );
 }
